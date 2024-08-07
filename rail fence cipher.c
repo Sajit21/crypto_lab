81fba7f6aaba1f2ac99814cc0201b65a100eb283
Sajit Maharjan <126838978+Sajit21@users.noreply.github.com>
2024-08-07 19:53:38 +0545

    Add files via upload

---
+++ rail fence cipher.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+void rail_fence_encrypt(char* plaintext, int rails) {
+    int len = strlen(plaintext);
+    char** fence = (char**)malloc(rails * sizeof(char*));
+    for (int i = 0; i < rails; i++) {
+        fence[i] = (char*)malloc(len * sizeof(char));
+        memset(fence[i], ' ', len * sizeof(char));
+    }
+
+    int row = 0, dir = 1;
+    for (int i = 0; i < len; i++) {
+        fence[row][i] = plaintext[i];
+        row += dir;
+        if (row == 0 || row == rails - 1)
+            dir = -dir;
+    }
+
+    printf("Encrypted text: ");
+    for (int i = 0; i < rails; i++) {
+        for (int j = 0; j < len; j++) {
+            if (fence[i][j] != ' ')
+                printf("%c", fence[i][j]);
+        }
+    }
+    printf("\n");
+
+    for (int i = 0; i < rails; i++) {
+        free(fence[i]);
+    }
+    free(fence);
+}
+
+int main() {
+    char plaintext[100];
+    int rails;
+
+    printf("Enter plaintext: ");
+    fgets(plaintext, sizeof(plaintext), stdin);
+    plaintext[strcspn(plaintext, "\n")] = 0;
+
+    printf("Enter number of rails: ");
+    scanf("%d", &rails);
+
+    rail_fence_encrypt(plaintext, rails);
+
+    return 0;
+}
+
